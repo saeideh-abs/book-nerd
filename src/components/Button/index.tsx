09e7b6b176c1c3a47eee598210b5082214f752b3
saeideh abbaszadeh <saeideh676@gmail.com>
2023-12-29 14:42:54 +0330

    add pressed state

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -41,13 +41,14 @@ export const Button = forwardRef<
       secondary: `text-secondary-500 hover:text-secondary-600`,
     },
     solid: {
-      primary: `bg-primary-500 text-primary-contrast hover:enabled:bg-primary-600`,
-      secondary: `bg-secondary-500 text-secondary-contrast hover:enabled:bg-secondary-600`,
+      primary: `bg-primary-500 text-primary-contrast hover:enabled:bg-primary-600 active:enabled:bg-primary-700`,
+      secondary: `bg-secondary-500 text-secondary-contrast hover:enabled:bg-secondary-600 active:enabled:bg-secondary-700`,
     },
     outlined: {
-      primary: `border-primary-500 text-primary-500 hover:border-primary-600 hover:text-primary-600 hover:enabled:bg-primary-50/50`,
+      primary: `border-primary-500 text-primary-500 hover:border-primary-600 hover:text-primary-600
+        hover:enabled:bg-primary-50/50 active:enabled:bg-primary-50`,
       secondary: `border-secondary-500 text-secondary-500 hover:border-secondary-600 hover:text-secondary-600
-        hover:enabled:bg-secondary-50/50 `,
+        hover:enabled:bg-secondary-50/50 active:enabled:bg-secondary-50`,
     },
   }
 
