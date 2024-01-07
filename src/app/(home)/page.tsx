796049e7150088b534b435636e1d3dea80546609
saeideh abbaszadeh <saeideh676@gmail.com>
2024-01-07 15:44:26 +0330

    get book data from db and show in carousel

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,44 @@
-import { BooksCarousel } from '@/components'
+'use client'
+import { supabaseClient } from '@/utils/supabaseClient'
+import { BooksCarousel, BooksCarouselProps } from '@/components'
 import Categories from './_components/Categories'
+import { useCallback, useEffect, useState } from 'react'
+import { Tables } from '@/types/database'
 
 const Home = () => {
+  const [booksData, setBooksData] = useState<BooksCarouselProps['items']>([])
+
+  const getData = useCallback(async () => {
+    const { data, error, status } = await supabaseClient
+      .from('book')
+      .select('*')
+      .limit(20)
+
+    setBooksData(formatData(data))
+    console.log(data, error, status)
+  }, [])
+
+  useEffect(() => {
+    getData()
+  }, [getData])
+
+  const formatData = (
+    data: Tables<'book'>[] | null,
+  ): BooksCarouselProps['items'] => {
+    return data
+      ? data.map((item: Tables<'book'>) => ({
+          title: item.title ?? '',
+          price: item.price,
+          authorName: 'Julia Robert',
+          coverImg: item.cover_img,
+        }))
+      : []
+  }
+
   return (
     <div className="flex flex-col items-center justify-center gap-10">
       <Categories />
-      <BooksCarousel />
+      <BooksCarousel items={booksData} />
     </div>
   )
 }
---
+++ src/components/BookBox/index.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { cn, customImageLoader } from '@/utils'
+import Image from 'next/image'
+import { Typography } from '../Typography'
+import { PropsWithChildren } from 'react'
+
+export interface BookBoxItemType {
+  title: string
+  price: number | null
+  authorName: string
+  coverImg: string
+}
+
+export interface BookBoxProps {
+  item: BookBoxItemType
+}
+
+export function BookBox({ item }: BookBoxProps) {
+  return (
+    <Card>
+      <Image
+        src={item.coverImg}
+        width={0}
+        height={0}
+        sizes="(min-width: 768px) 50vw, (min-width: 1024px) 25vw,(min-width: 1280px) 25vw, 100vw"
+        className="w-[200px] h-auto"
+        alt=""
+        loader={customImageLoader}
+      />
+      <div className="flex flex-col justify-end grow">
+        <Typography variant="textLgSemibold" className="mt-3 text-primary-500">
+          {item.title}
+        </Typography>
+        <Typography variant="textSmReg" className="text-bnGray-400">
+          {item.authorName}
+        </Typography>
+        <Typography
+          variant="textMdBold"
+          className={cn('mt-3 text-secondary-500', !item.price && 'invisible')}
+        >
+          {item.price ? `$${item.price}` : 'unknown'}
+        </Typography>
+      </div>
+    </Card>
+  )
+}
+
+const Card = ({ children }: PropsWithChildren) => {
+  return (
+    <div
+      className={cn(
+        `flex flex-col items-center p-6 border text-center border-gray-200 bg-white rounded-sm 
+                    w-full h-full shadow-[0px_4px_10px_0px_rgba(0,0,0,0.15)]`,
+      )}
+    >
+      {children}
+    </div>
+  )
+}
